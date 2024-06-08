3abefe0f15917e9556b64f4f14417919e949c820
AdielDiaz <adielalvarez02@gmail.com>
2024-06-08 16:50:17 -0600

    Confirmar

--- proyectoagua/resources/views/home.blade.php
+++ proyectoagua/resources/views/home.blade.php
@@ -16,6 +16,38 @@
             <li><a href="{{ route('recursoshidricos.index') }}" class="btn btn-primary">Recursos Hídricos</a></li>
             <li><a href="{{ route('fuentedecontaminacion.index') }}" class="btn btn-primary">Fuente de Contaminación</a></li>
         </ul>
+        <h2>Contaminacion del agua en El Salvador.</h2>
+        <p>La contaminación del agua en El Salvador es un desafío crítico que afecta la salud pública y el medio ambiente. Las descargas de aguas residuales sin tratar, la agricultura intensiva y la actividad industrial son las principales causas de este problema. Esto resulta en la contaminación química, bacteriológica y la eutrofización de los cuerpos de agua, poniendo en riesgo la biodiversidad acuática y la salud de las comunidades que dependen de estos recursos. Para abordar esta crisis, se requiere una acción coordinada que incluya mejoras en la infraestructura de saneamiento, regulaciones ambientales más estrictas y programas de educación ambiental para promover prácticas sostenibles de gestión del agua.</p>
+        <br>
+        <h2>Causas de la contaminacion del agua en El Salvdor.</h2>
+        <p>1. Contaminación del agua: La descarga de aguas residuales sin tratar en ríos y cuerpos de agua es una causa importante de contaminación del agua. Además, la deforestación y la agricultura intensiva contribuyen a la erosión del suelo, lo que lleva a la sedimentación y la contaminación del agua.</p>
+        <p>2. Contaminación del aire: La quema de residuos sólidos, la emisión de gases de vehículos y la actividad industrial son las principales fuentes de contaminación del aire en El Salvador. Las emisiones de dióxido de azufre y material particulado están asociadas con problemas respiratorios y de salud pública.</p>
+        <p>3. Contaminación del suelo: El uso de pesticidas y fertilizantes químicos en la agricultura, así como la disposición inadecuada de desechos sólidos, contribuyen a la contaminación del suelo. Esto puede afectar la calidad del suelo y la salud de las personas que dependen de la agricultura para su sustento.</p>
+        <div class=image-container>
+            <img src="imagen/imagen1.jpg" alt="Contaminacion 1">
+        </div>
+        <br>
+        <h2>Consecuencias de la contaminación del agua en El Salvador.</h2>
+        <p>1. Impacto en la salud: La contaminación del agua, el aire y el suelo puede causar una variedad de problemas de salud, como enfermedades respiratorias, problemas gastrointestinales, cáncer y problemas de la piel. Las poblaciones más vulnerables, como los niños y los ancianos, son especialmente afectadas.</p>
+        <p>2. Degradación del medio ambiente: La contaminación afecta la biodiversidad y los ecosistemas naturales, lo que puede llevar a la pérdida de hábitats y la extinción de especies. La contaminación del agua también afecta la disponibilidad de agua potable y los ecosistemas acuáticos.</p>
+        <p>3. Impacto económico: La contaminación puede tener un impacto negativo en la economía, ya que afecta sectores como la agricultura, el turismo y la pesca. Además, los costos asociados con el tratamiento de enfermedades causadas por la contaminación pueden ser significativos para el sistema de salud.</p>
+        <div class=image-container>
+            <img src="imagen/imagen2.jpg" alt="Contaminacion 2">
+        </div>
+        <br>
+        <h2>Medidas para erradicar la contaminación del agua en El Salvador:</h2>
+        <p>1. Mejora de la gestión de residuos: Implementar políticas y programas para la gestión adecuada de residuos sólidos, incluyendo la recolección, reciclaje y disposición final adecuada de residuos.</p>
+        <p>2. Tratamiento de aguas residuales: Invertir en infraestructuras para el tratamiento de aguas residuales y promover prácticas de saneamiento adecuadas para reducir la contaminación del agua.</p>
+        <p>3. Promoción de energías limpias: Fomentar el uso de fuentes de energía renovable y tecnologías más limpias para reducir las emisiones de gases contaminantes.</p>
+        <p>4. Regulación ambiental: Fortalecer y hacer cumplir las regulaciones ambientales para controlar las emisiones industriales, la deforestación y otras actividades que contribuyen a la contaminación.</p>
+        <p>5. Educación y concienciación: Promover la educación ambiental y la sensibilización pública sobre los impactos de la contaminación y la importancia de prácticas sostenibles.</p>
+        <div class=image-container>
+            <img src="imagen/imagen4.png" alt="Soluciones de la contaminacion">
+        </div>
+        <br>
+        <p>La contaminación del agua en El Salvador es un desafío multifacético que requiere atención urgente y acción concertada. Sus causas, que van desde la descarga de aguas residuales sin tratar hasta la agricultura intensiva y la actividad industrial, tienen impactos significativos en la salud humana, la biodiversidad y la economía. Sin embargo, a través de la implementación de medidas adecuadas, como mejoras en la infraestructura de saneamiento, regulaciones ambientales más estrictas y programas de educación ambiental, es posible abordar este problema y proteger los valiosos recursos hídricos de El Salvador para las generaciones presentes y futuras. Es esencial un compromiso conjunto de todos los sectores de la sociedad para lograr un cambio positivo y duradero en la gestión y preservación del agua en el país.</p>
+        <br>
+        <p class=derechos>Derechos de autor: © Adiel Edgardo Diaz Alvarez</p>
     </div>
 </body>
 </html>
